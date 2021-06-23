5ac3402b1c226b91f2c9b19ddbb1d3184e94d27b
raton <raton3062015@gmail.com>
2021-06-23 08:58:54 +0600

    cpp update

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -3,18 +3,34 @@ using namespace std;
 //Find the smallest missing positive number from an unsorted array
 // 1 8 9 2 2 4 6 5 7
 
+int parti(int ar[],int n){
+	int p=0;
+	for(int i=0;i<n;++i){
+		if(ar[i]>=0)swap(ar[i],ar[p++]);
+	
+	}
+	return p;
+}
+
+
 int main(){
 	//vectorint> v={1,2};
 	 int arr[] = { 1, 4, 2, -1, 6, 5 };
 	 int n = sizeof(arr) / sizeof(arr[0]);
-	unordered_set<int> s(arr,arr+n);
-	int ind=1;
-	while(true){
-		if(!s.count(ind)){
-			cout << ind <<endl; 
-			break;
-		}
-		++ind;
+	int p= parti(arr,n);
+	//for(int i=0;i<n;++i)cout<< arr[i]<<" ";
+	for(int i=0;i<p;++i){
+		arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
 	}
-	return 0;
+	
+	for(int i=0;i<p;++i){
+		cout << arr[i] <<" ";
+	}	
+	cout <<endl;
+	int i;
+	for( i=0;i<p;++i){
+		if(arr[i]>=0)break;
+		
+	}
+	cout << i+1 <<endl;
 }
